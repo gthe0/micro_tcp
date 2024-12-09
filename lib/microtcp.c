5d374711bd56ace1e36924f8a9388064944586a5
gtheo <giorgostheodoraki@hotmail.com>
2024-12-09 11:13:49 +0200

    Tested parser and removed Your code here comments

--- lib/microtcp.c
+++ lib/microtcp.c
@@ -22,68 +22,52 @@
  */
 
 #include "microtcp.h"
-#include <crc32.h>
 
-#include <netinet/in.h>
-#include <time.h>
 #include <assert.h>
+#include <crc32.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <time.h>
 
-
-microtcp_sock_t
-microtcp_socket (int domain, int type, int protocol)
-{
+microtcp_sock_t microtcp_socket(int domain, int type, int protocol) {
     microtcp_sock_t socket_obj;
-    memset(&socket_obj,0,sizeof(microtcp_sock_t));
+    memset(&socket_obj, 0, sizeof(microtcp_sock_t));
 
     srand(time(NULL));
     socket_obj.seq_number = rand();
 
-    if((socket_obj.sd = socket(domain,type,protocol)) == -1)
+    if ((socket_obj.sd = socket(domain, type, protocol)) == -1)
         socket_obj.state = INVALID;
-    
+
     return (socket_obj);
 }
 
-int
-microtcp_bind (microtcp_sock_t *socket, const struct sockaddr *address,
-               socklen_t address_len)
-{
-  /* Your code here */
+int microtcp_bind(microtcp_sock_t *socket, const struct sockaddr *address,
+                  socklen_t address_len) {
+    return 0;
 }
 
-int
-microtcp_connect (microtcp_sock_t *socket, const struct sockaddr *address,
-                  socklen_t address_len)
-{
-  /* Your code here */
+int microtcp_connect(microtcp_sock_t *socket, const struct sockaddr *address,
+                     socklen_t address_len) {
+    return 0;
 }
 
-int
-microtcp_accept (microtcp_sock_t *socket, struct sockaddr *address,
-                 socklen_t address_len)
-{
-  /* Your code here */
+int microtcp_accept(microtcp_sock_t *socket, struct sockaddr *address,
+                    socklen_t address_len) {
+    return 0;
 }
 
-int
-microtcp_shutdown (microtcp_sock_t *socket, int how)
-{
-  /* Your code here */
+int microtcp_shutdown(microtcp_sock_t *socket, int how) { return 0;}
+
+ssize_t microtcp_send(microtcp_sock_t *socket, const void *buffer,
+                      size_t length, int flags) {
+    return 0;
 }
 
-ssize_t
-microtcp_send (microtcp_sock_t *socket, const void *buffer, size_t length,
-               int flags)
-{
-  /* Your code here */
-}
-
-ssize_t
-microtcp_recv (microtcp_sock_t *socket, void *buffer, size_t length, int flags)
-{
-  /* Your code here */
+ssize_t microtcp_recv(microtcp_sock_t *socket, void *buffer, size_t length,
+                      int flags) {
+    return 0;
 }
